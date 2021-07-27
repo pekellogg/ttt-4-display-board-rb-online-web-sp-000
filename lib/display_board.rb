16345997e0da0cdbe21ff2ae940b9a1d5f77cc1d
Peyton <pekellogg@hotmail.com>
2021-07-27 16:38:48 -0700

    test

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,4 @@
-# Define display_board that accepts a board and prints
-# out the current state.
 
-board = []
 
 def display_board(board)
     puts " #{board[0]} | #{board[1]} | #{board[2]} "
@@ -11,4 +8,3 @@ def display_board(board)
     puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-display_board(board)
