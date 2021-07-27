a29a8f18d9141e1eb7350271d26a7cb6ca8a1c78
Peyton <pekellogg@hotmail.com>
2021-07-27 16:36:29 -0700

    commit tree 6af360f3888a955bbcfdf634b226dcc2da1b2617

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,8 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
+board = []
+
 def display_board(board)
     puts " #{board[0]} | #{board[1]} | #{board[2]} "
     puts "-----------"
@@ -8,3 +10,5 @@ def display_board(board)
     puts "-----------"
     puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+display_board(board)
